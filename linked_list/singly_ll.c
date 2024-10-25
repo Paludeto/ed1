bde6f642c4c267fc1b8732374c96e2fdeac8afc5
Paludeto <gpalu54@gmail.com>
2024-10-25 09:07:17 -0300

    aula 25/10

--- linked_list/singly_ll.c
+++ linked_list/singly_ll.c
@@ -12,7 +12,6 @@ typedef struct Objeto {
 
 typedef struct LinkedList {
     Objeto *inicio;
-    Objeto *fim;
     int tamanho;
 } LinkedList;
 
@@ -26,6 +25,17 @@ void inicializaLista(LinkedList *lista) {
 bool estaVazia(LinkedList *lista) {
 
     return lista->tamanho == 0;
+
+}
+
+void imprimeLista(LinkedList *lista) {
+
+    Objeto *obj = lista->inicio;
+
+    while (obj != NULL) {
+        printf("%s\n", obj->nome);
+        obj = obj->prox;
+    }
 
 }
 
@@ -44,7 +54,7 @@ void insereElemento(LinkedList *lista, int cpf, int idade, char nome[]) {
     novoObj->prox = NULL;
 
     if (estaVazia(lista)) {
-        lista->inicio = lista->fim = novoObj;
+        lista->inicio =  novoObj;
     } else {
 
         // Ordem crescente de idade
@@ -56,17 +66,63 @@ void insereElemento(LinkedList *lista, int cpf, int idade, char nome[]) {
 
             Objeto *auxiliar = lista->inicio;
 
-            while (novoObj->idade < auxiliar->prox->idade && auxiliar->prox != NULL) {
+            while (auxiliar->prox != NULL && novoObj->idade > auxiliar->prox->idade) {
                 auxiliar = auxiliar->prox;
             }
 
-            
+            novoObj->prox = auxiliar->prox;
+            auxiliar->prox = novoObj;  
 
         }
 
+        lista->tamanho++;   
         
+    }   
+    
+}
+
+void removeElemento(LinkedList *lista, int idade) {
+
+    if (estaVazia(lista)) {
+        printf("Lista vazia!\n");
+        return;
     }
 
+    if (idade < lista->inicio->idade) {
+        printf("Objeto não existe\n");
+        return;
+    }
+
+    // Terminar!
+
+} 
+
+void destroiLista(LinkedList *lista) {
+
+    Objeto *aux = lista->inicio;
+    Objeto *aux2;    
+
+    while (aux->prox != NULL) {
+        aux2 = aux;        
+        aux = aux->prox;
+        free(aux2);
+    }
+
+}   
+
+// Fazer pesquisa, mínimo, máximo, próximo e anterior
+int main() {
     
+    LinkedList lista;
+
+    insereElemento(&lista, 12345, 38, "Pietra");
+    insereElemento(&lista, 54321, 60, "Pietr");
+    insereElemento(&lista, 87654, 70, "Piet");
+    insereElemento(&lista, 34567, 62, "Pie");
     
+    imprimeLista(&lista);
+    destroiLista(&lista);
+
+    return 0;
+
 }
