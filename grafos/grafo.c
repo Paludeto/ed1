5892843dcf671d763ab3a9d5ee4a7a8b7d399427
Paludeto <gpalu54@gmail.com>
2025-02-18 23:21:20 -0300

    dfs

--- grafos/grafo.c
+++ grafos/grafo.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// Fila
+
 typedef struct No {
     int vertice;
     struct No *prox;
 } No;
 
+// Pilha
+typedef struct Pilha {
+    No *topo;
+    int qtde_pilha;
+} Pilha;
+
+// Fila
 typedef struct Fila {
     No *inicio_fila;
     No *fim_fila;
@@ -20,6 +27,50 @@ typedef struct Grafo {
     int num_vert;
     bool digrafo;
 } Grafo;
+
+void inicializa_pilha(Pilha *pilha) {
+
+    pilha->topo = NULL;
+    pilha->qtde_pilha = 0;
+
+}
+
+bool pilha_vazia(Pilha *pilha) {
+
+    return pilha->qtde_pilha == 0;
+
+}
+
+void empilha(Pilha *pilha, int vertice) {
+
+    No *novo_no = (No *) malloc(sizeof(No));
+
+    novo_no->vertice = vertice;
+
+    if (!novo_no) {
+        perror("Erro de alocacao\n");
+        exit(EXIT_FAILURE);
+    }
+
+    novo_no->prox = pilha->topo;
+    
+    pilha->topo = novo_no;
+    pilha->qtde_pilha++;
+
+}
+
+int desempilha(Pilha *pilha) {
+
+    No *aux = pilha->topo;
+
+    pilha->topo = aux->prox;
+    int vert = aux->vertice;
+    free(aux);
+    pilha->qtde_pilha--;
+
+    return vert;
+
+}
 
 // Fila
 void inicializa_fila(Fila *fila) {
@@ -103,7 +154,6 @@ void inicializa_grafo(Grafo *grafo, int num_vert, bool digrafo) {
 
 void adiciona_aresta(Grafo *grafo, int src, int dest) {
 
-    // se nao eh digrafo
     grafo->matriz_adj[src][dest] = 1;
 
     if (!grafo->digrafo) {
@@ -123,7 +173,7 @@ void imprime_matriz(Grafo *grafo) {
 
 }
 
-void bfs(Grafo *grafo, int vertice) {
+void bfs(Grafo *grafo, int src) {
 
     int visitados[grafo->num_vert];
 
@@ -133,13 +183,13 @@ void bfs(Grafo *grafo, int vertice) {
 
     Fila fila;
     inicializa_fila(&fila);
-    visitados[vertice] = 1;
-    adiciona_fila(&fila, vertice);
+    visitados[src] = 1;
+    adiciona_fila(&fila, src);
     
     while (!fila_vazia(&fila)) {
 
         int vertice = remove_fila(&fila);
-        printf("%d\n", vertice);
+        printf("%d ", vertice);
 
         for (int i = 0; i < grafo->num_vert; i++) {
             if (grafo->matriz_adj[vertice][i] == 1) {
@@ -163,35 +213,100 @@ void bfs(Grafo *grafo, int vertice) {
 
     }
 
+    printf("\n");
+
+}
+
+void dfs(Grafo *grafo, int src) {
+
+    Pilha pilha;
+    inicializa_pilha(&pilha);
+
+    int visitados[grafo->num_vert];
+
+    for (int i = 0; i < grafo->num_vert; i++) {
+        visitados[i] = 0;
+    }
+
+    empilha(&pilha, src);
+    visitados[src] = 1;
+
+    while (!pilha_vazia(&pilha)) {
+
+        int vertice = desempilha(&pilha);
+        printf("%d ", vertice);
+
+        for (int i = 0; i < grafo->num_vert; i++) {
+            if (grafo->matriz_adj[vertice][i] == 1) {
+                if (visitados[i] == 0) {
+                    empilha(&pilha, i);
+                    visitados[i] = 1;
+                }
+            }
+        }
+
+    }
+
+    // vertices desconexos
+    for (int i = 0; i < grafo->num_vert; i++) {
+        if (!visitados[i]) {
+            empilha(&pilha, i);
+            visitados[i] = 1;
+            while (!pilha_vazia(&pilha)) {
+                int vert = desempilha(&pilha);
+                printf("%d ", vert);
+                for (int j = grafo->num_vert - 1; j >= 0; j--) {
+                    if (grafo->matriz_adj[vert][j] == 1 && !visitados[j]) {
+                        empilha(&pilha, j);
+                        visitados[j] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("\n");
+
 }
 
 int main() {
 
     Grafo grafo;
-    Fila fila;
 
-    inicializa_fila(&fila);
-    inicializa_grafo(&grafo, 6, false);
+    inicializa_grafo(&grafo, 7, false);
 
-    /*
-        0 -- 2
-        |    |
-        3 -- 5
-        |    |
-        1 -- 4
-    */  
+  /*
+         Graph Structure:
+         
+               0
+              / \
+             1   2
+             |    \
+             3     4
+              \   /
+                5
+                |
+                6
 
+         Edges:
+         0-1, 0-2, 1-3, 2-4, 3-5, 4-5, 5-6
+    */
+    adiciona_aresta(&grafo, 0, 1);
     adiciona_aresta(&grafo, 0, 2);
-    adiciona_aresta(&grafo, 0, 3);
     adiciona_aresta(&grafo, 1, 3);
-    adiciona_aresta(&grafo, 1, 4);
-    adiciona_aresta(&grafo, 2, 5);
+    adiciona_aresta(&grafo, 2, 4);
     adiciona_aresta(&grafo, 3, 5);
     adiciona_aresta(&grafo, 4, 5);
+    adiciona_aresta(&grafo, 5, 6);
 
+    printf("Matriz de adjacência: \n");
     imprime_matriz(&grafo);
+    printf("====================\n");
 
+    printf("BFS: ");
     bfs(&grafo, 0);
+    printf("DFS: ");
+    dfs(&grafo, 0);
 
     return 0;
 
